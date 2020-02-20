e4311e59c914fe3bc2a3361f3eaafbc32ce00bd1
lb-udes <55243135+lb-udes@users.noreply.github.com>
2020-02-20 15:27:15 -0500

    modifications de la manière de loader les questions

--- audit2/main.cpp
+++ audit2/main.cpp
@@ -2,54 +2,74 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
-
+#include "tinyxml2.h"
+#include <time.h>
 #include "Question.h"
 
 using namespace std;
-
+using namespace tinyxml2;
 
 int main()
 {
-	ifstream qns("questions.txt");
-	if (!qns.is_open())
-		return -1;
+	srand(int(time(NULL)));
 
 	Question question;
-	while (!qns.eof()) {
-		question.lire(qns);
+
+	Questions questions("questions.xml");
+
+	XMLError load = questions.load();
+
+	cout << XMLDocument::ErrorIDToName(load) << endl;
+
+	XMLError state = questions.read(question);
+
+	if (state != XML_SUCCESS)
+	{
+		cout << XMLDocument::ErrorIDToName(state) << endl;
+		return -1;
+	}
+
+	do {
 
 		cout << "***********************************************************\n"
 			<< "************ WHO WANTS TO BE A MILLIONAIRE ??? ************\n"
 			<< "***********************************************************\n"
 			<< endl;
 
-		cout << question.get() << endl << endl;
+		//cout << question.get() << endl << endl;
 
-		for (ushort i = 0; i < 4; ++i) {
-			cout << question.choix()[i] << endl;
-		}
+		//for (ushort i = 0; i < 4; ++i) {
+		//	cout << question.choix()[i] << endl;
+		//}
+
+		question.randomize();
+
+		cout << question;
 
 		char reponse = cin.get();
 		cin.get();//Ignorer la touche entree
 
-		//Convertir la reponse en lettre majuscule
-		if (reponse >= 97)//a = 97, A=65 en ASCII
-			reponse -= 32;
-
-		if (reponse == question.reponse()) {
+		if (question.validate(reponse)) {
 			cout << "BONNE REPONSE !!" << endl;
 		}
 		else {
 			cout << "Mauvaise reponse... :(\nVous avez perdu" << endl;
+			cout << "La bonne reponse etait : " << question.choix()[question.reponse()];
 			break;
 		}
 
 		cin.get();//Attendre que l'tilisateur appuie sur une touche pour continuer
 		system("CLS");
+
+		state = questions.read(question);
+
+	} while (state == XML_SUCCESS);
+	
+	if (state != XML_SUCCESS)
+	{
+		cout << XMLDocument::ErrorIDToName(state) << endl;
+		return -1;
 	}
-
-
-	qns.close();
 
 	cin.get();
 	return 0;
